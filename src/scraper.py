ba89527d3db689be37f4a5a8c3fadacd4605ea85
SAW <99191294+nicsaw@users.noreply.github.com>
2024-05-15 02:30:59 +1000

    Implement basic scraper

---
+++ src/scraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+def get_soup(url):
+    response = requests.get(url)
+    html = response.text
+    return BeautifulSoup(html, 'html.parser')
+
+def get_content_by_tag(tag):
+    elements = soup.find_all(tag)
+    return [element.text.strip() for element in elements]
+
+soup = get_soup('https://www.scrapethissite.com/pages/')
+
+titles = get_content_by_tag('h3')
+descriptions = get_content_by_tag('p')
+
+data = { 'Title': titles, 'Description': descriptions }
+df = pd.DataFrame(data)
+df.to_csv('output.csv', index=False)
